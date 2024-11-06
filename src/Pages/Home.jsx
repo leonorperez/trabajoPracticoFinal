b242c5456ec1ca949fe17abd1442a797de955bac
oliveralbo <oliveralbo@hotmail.com>
2024-11-06 19:59:01 -0300

    feat: react query working ok

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,9 @@
+import { useGetPokemon } from '../services/services';
 export const Home = () => {
+  const { data } = useGetPokemon();
+
+  console.log(data);
+
   return (
     <div>
       <h2 className="text-blue-700">Home</h2>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
+import { QueryClientProvider } from '@tanstack/react-query'; // provider que nos provee react query, que va a usarse en toda la app
+import queryClient from './services/queryClient.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
