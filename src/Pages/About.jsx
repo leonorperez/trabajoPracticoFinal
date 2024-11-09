62cbb595fe8690946db27bee64e3dc5022cc7c1a
leandro7d <111540213+leandro7d@users.noreply.github.com>
2024-11-09 12:27:43 -0300

    Update About.jsx
    Agregando participantes

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,59 @@
 export const About = () => {
   return (
-    <div className="px-10">
+    
+    <div className="flex flex-row p-4 bg-gradient-to-r from-black to-blue-900 shadow-lg shadow-cyan-500 ">
+      <div className="basis-1/4 mr-2 h-80 font-bold text-center font-mono text-lg text-white ">
+      <img 
+          src="https://iili.io/2I83QCF.png"
+          alt="Pokemon Logo"
+          className="scale-75"
+        />Gaspy</div>
+      
+      <div className=" basis-1/4 mr-2  h-80 font-bold text-center font-mono text-lg text-white  ">
+      <img 
+          src="https://iili.io/2I8JfeI.png"
+          alt="Pokemon Logo"
+          className="scale-75 "
+        />Lean</div>
+
+      <div className="basis-1/4 mr-2  h-80 font-bold text-center font-mono text-lg text-white ">
+      <img 
+          src="https://iili.io/2I8t1xj.png"
+          alt="Pokemon Logo"
+          className="scale-75 "
+        />Dany</div>
+
+      <div className="basis-1/4 mr-2  h-80 font-bold text-center font-mono text-lg text-white ">
+      <img 
+          src="https://iili.io/2I8U61s.png"
+          alt="Pokemon Logo"
+          className="scale-75 "
+        />Ivi</div>
+
+      <div className="basis-1/4 mr-2  h-80 font-bold text-center font-mono text-lg text-white ">
+      <img 
+          src="https://iili.io/2ISJBt9.png"
+          alt="Pokemon Logo"
+          className="scale-75 "
+        />Juli</div>
+
+      <div className="basis-1/4 mr-2  h-80 font-bold text-center font-mono text-lg text-white ">
+      <img 
+          src="https://iili.io/2I8cMep.png"
+          alt="Pokemon Logo"
+          className="scale-75 "
+        />Leo</div>
+        
+      <div className="basis-1/4 mr-2  h-80 font-bold text-center font-mono text-lg text-white ">
+      <img 
+          src="https://iili.io/2I8WHAb.png"
+          alt="Pokemon Logo"
+          className="scale-75 "
+        />Oli</div>
+
+      {/*
       <h2>About us...</h2>
-      <ul className="list-disc pl-6">
+      <ul className="list-disc pl-6 " >
         <li>Gaspy</li>
         <li>Lean</li>
         <li>Dany</li>
@@ -10,7 +61,9 @@ export const About = () => {
         <li>Juli</li>
         <li>Leo</li>
         <li>Oli</li>
-      </ul>
+      </ul> */}
+
+      
     </div>
   );
 };
