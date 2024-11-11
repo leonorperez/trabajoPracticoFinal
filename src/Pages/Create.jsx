1998ffd698f3c6e1855e635416648474d4cf6876
oliveralbo <oliveralbo@hotmail.com>
2024-11-10 22:36:32 -0300

    create

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,7 +1,97 @@
+import React, { useState } from 'react';
+import { useCreatePokemon } from '../services/services';
+
 export const Create = () => {
+  const { mutate: createPokemon } = useCreatePokemon();
+  const [data, setData] = useState({
+    name: null,
+    order: null,
+    weight: 0,
+    image: null,
+    types: [],
+    team: 1,
+  });
+  const [nuevoPokemon, setNuevoPokemon] = useState(null);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    console.log(data);
+    createPokemon(data, {
+      onSuccess: res => {
+        setNuevoPokemon(res);
+      },
+      onError: err => console.log(err?.response?.data?.message),
+    });
+  };
+
+  function handleInput(event) {
+    const field = {
+      name: event.target.name,
+      value: event.target.value,
+    };
+    setData({
+      ...data,
+      [field.name]: field.value,
+    });
+  }
+
+  const PokemonTypesEnum = {
+    BUG: 'Bug',
+    DARK: 'Dark',
+    DRAGON: 'Dragon',
+    ELECTRIC: 'Electric',
+    FAIRY: 'Fairy',
+    FIGHTING: 'Fighting',
+    FIRE: 'Fire',
+    FLYING: 'Flying',
+    GHOST: 'Ghost',
+    GRASS: 'Grass',
+    GROUND: 'Ground',
+    ICE: 'Ice',
+    NORMAL: 'Normal',
+    POISON: 'Poison',
+    PSYCHIC: 'Psychic',
+    ROCK: 'Rock',
+    STEEL: 'Steel',
+    WATER: 'Water',
+  };
+
+  console.log(nuevoPokemon && nuevoPokemon);
+  // console.log('data ' && data);
+
   return (
-    <div>
-      <h2>Aca avoy a havser el form</h2>
-    </div>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col w-1/3 mx-auto gap-5 p-6 border-2 border-black bg-slate-200"
+    >
+      <label htmlFor="">
+        Nombre
+        <input name="name" type="text" onChange={event => handleInput(event)} />
+      </label>
+      <label htmlFor="">
+        Orden
+        <input name="order" type="number" onChange={event => handleInput(event)} />
+      </label>
+      <label htmlFor="">
+        weight
+        <input name="weight" type="number" onChange={event => handleInput(event)} />
+      </label>
+      <label htmlFor="">
+        image
+        <input name="image" type="text" onChange={event => handleInput(event)} />
+      </label>
+      <label htmlFor="">
+        types
+        <select name="types">
+          {Object.values(PokemonTypesEnum).map(type => (
+            <option key={type} value={type}>
+              {type}
+            </option>
+          ))}
+        </select>
+      </label>
+      <button type="submit">Enviar</button>
+    </form>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,14 +9,29 @@ export const Card = ({ link, pokeFacu }) => {
   }
   const finalPokemon = pokemon ? pokemon : pokeFacu;
 
+  if (finalPokemon.name === 'Pikab') {
+    console.log(finalPokemon);
+  }
+
   return (
     <Link to={`/pokemon/${finalPokemon.id}`} state={{ pokemon }}>
-      <div className="border rounded-lg p-4 shadow-md  min-w-60 min-h-60 m-2 flex flex-col justify-center">
+      <div className="border rounded-lg p-4 shadow-md  min-w-60 min-h-80 m-2 flex flex-col justify-between">
         <div className="text-center text-lg">{finalPokemon?.name}</div>
-        <img src={finalPokemon.image} width="200px" />
-        <div className="text-center">Peso: {finalPokemon?.weight}</div>
-        <div className="text-center">
-          {`${finalPokemon.types.length > 1 ? 'Tipos' : 'Tipo'}:`} {finalPokemon?.types.join(', ')}
+
+        <img
+          src={finalPokemon.image}
+          width="200px"
+          height="200px"
+          onError={e => (e.target.src = 'https://i.imgur.com/ygIMkZv.png')}
+          alt="Imagen no disponible"
+        />
+
+        <div>
+          <div className="text-center">Peso: {finalPokemon?.weight}</div>
+          <div className="text-center">
+            {`${finalPokemon.types.length > 1 ? 'Tipos' : 'Tipo'}:`}{' '}
+            {finalPokemon?.types.join(', ')}
+          </div>
         </div>
       </div>
     </Link>
